79d3a704f37e036ac71e82418b27b94306ef2ae6
ConnorDBurge <connor.d.burge@gmail.com>
2022-03-02 19:57:47 -0800

    changed transition timing on cover

--- handlers.js
+++ handlers.js
@@ -1,13 +1,17 @@
 const expand = (index) => {
     const section = document.getElementById(`hidden${index}`);
     const seeButton = document.getElementById(`seemore${index}`);
-    if (seeButton.innerHTML == 'See more') {
-        section.classList.remove('hidden');
+
+    section.classList.toggle("hidden");
+    const hidden = section.classList.contains("hidden");
+
+    if (hidden) {
         seeButton.innerHTML = 'Close'
         seeButton.classList.add('open');
+        section.style.transition = 'max-height 0.7s linear';
     } else {
-        section.classList.add('hidden');
         seeButton.innerHTML = 'See more';
         seeButton.classList.remove('open');
+        section.style.transition = 'max-height 0.5s linear';
     }
 }
