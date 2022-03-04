f1bf1aa75fba88ae1e42e2632c2daabbb370235a
ConnorDBurge <connor.d.burge@gmail.com>
2022-03-03 21:03:43 -0800

    fixed sroll button onload

--- observers.js
+++ observers.js
@@ -1,3 +1,9 @@
+window.onload = (event) => {
+    const downButton = document.querySelector('.cover-photo__scroll')
+    downButton.classList.add('fader');
+    downButton.classList.add('appear');
+};
+
 // Appear class Intersection Observer
 const appearOptions = {
     rootMargin: "0px 0px -30px 0px"
